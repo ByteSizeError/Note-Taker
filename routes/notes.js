8a022b2ba08d1c49de1bc734f2db63bad1cc030b
Jimmy Zheng <jimmyz0622@gmail.com>
2021-10-16 16:39:46 -0700

    delete api added

--- routes/notes.js
+++ routes/notes.js
@@ -1,28 +1,76 @@
 const notes = require('express').Router();
-const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const fs = require('fs');
 const uniqid = require('uniqid');
+const notesDB = "./db/db.json";
 
 notes.get('/', (req, res) => {
-    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+    fs.readFile(notesDB, 'utf8', (error, data) => {
+        if (error) {
+            console.error(error);
+        }
+        else {
+            res.json(JSON.parse(data));
+        }
+    });
 });
 
 notes.post('/', (req, res) => {
-    console.log(req.body);
-
     const { title, text } = req.body;
 
     if (req.body) {
         const newNote = {
             title,
             text,
-            note_id: uniqid(),
+            id: uniqid(),
         };
 
-        readAndAppend(newNote, './db/db.json');
+        fs.readFile(notesDB, 'utf8', (error, data) => {
+            if (error) {
+                console.error(error);
+            }
+            else {
+                const parsedData = JSON.parse(data);
+                parsedData.push(newNote);
+
+                fs.writeFile(notesDB, JSON.stringify(parsedData, null, 4), (error) => {
+                    if (error) {
+                        console.error(error);
+                    }
+                    else {
+                        console.info(`\nNote ${JSON.stringify(newNote)} written to ${notesDB}`);
+                    }
+                });
+            }
+        });
         res.json(`Note added successfully 🚀`);
-    } else {
+    }
+    else {
         res.error('Error in adding note');
     }
+});
+
+notes.delete('/:id', (req, res) => {
+    fs.readFile(notesDB, 'utf8', (error, data) => {
+        if (error) {
+            console.error(error);
+        } else {
+            const parsedData = JSON.parse(data);
+            for (i in parsedData) {
+                if (parsedData[i].id == req.params.id) {
+                    parsedData.splice(i, 1);
+                }
+            }
+            fs.writeFile(notesDB, JSON.stringify(parsedData, null, 4), (error) => {
+                if (error) {
+                    console.error(error);
+                }
+                else {
+                    console.info(`\nNote id:"${req.params.id}" removed from ${notesDB}`);
+                }
+            });
+            res.json(`Note ${req.params.id} removed successfully 🚀`);
+        }
+    });
 });
 
 module.exports = notes;
